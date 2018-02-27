cfa38741a69a244829143662778a8319ddb7bfcf
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-02-27 09:44:47 +0000

    Add support for creating a new session and closing it

--- session.go
+++ session.go
@@ -18,8 +18,48 @@ package sqlite3
 #include <stdlib.h>
 */
 import "C"
+import (
+	"runtime"
+	"unsafe"
+)
 
 // SQLiteSession implement interface of Session.
 type SQLiteSession struct {
 	s *C.sqlite3_session
+}
+
+// Session creates a new session attached to the given database of this
+// connection.
+//
+// Session objects must be closed before the SQLiteConn they are attached to is
+// closed.
+func (c *SQLiteConn) Session(database string) (*SQLiteSession, error) {
+	databaseptr := C.CString(database)
+	defer C.free(unsafe.Pointer(databaseptr))
+
+	session := &SQLiteSession{}
+	rv := C.sqlite3session_create(c.db, databaseptr, &session.s)
+
+	if rv != C.SQLITE_OK {
+		return nil, c.lastError()
+	}
+
+	runtime.SetFinalizer(session, (*SQLiteSession).Finish)
+	return session, nil
+}
+
+// Finish deletes the session.
+func (s *SQLiteSession) Finish() error {
+	return s.Close()
+}
+
+// Close deletes the session.
+func (s *SQLiteSession) Close() error {
+	// sqlite3session_delete() never fails.
+	C.sqlite3session_delete(s.s)
+
+	s.s = nil
+	runtime.SetFinalizer(s, nil)
+
+	return nil
 }
--- session_test.go
+++ session_test.go
@@ -6,11 +6,61 @@
 
 package sqlite3
 
-import "testing"
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+)
 
 func TestSessionUninitialized(t *testing.T) {
 	session := SQLiteSession{}
 	if session.s != nil {
 		t.Fatal("Expected reference to sqlite3_session to be nil")
 	}
+}
+
+func TestSessionOpenAndClose(t *testing.T) {
+	_, _, cleanup := testSessionOpen(t, "openAndClose")
+	defer cleanup()
+}
+
+// Create a SQLiteSession object attached to an in-memory test database.
+//
+// Return the test database, the newly created session and a cleanup function
+// to safely close both the session and the database.
+func testSessionOpen(t *testing.T, test string) (*sql.DB, *SQLiteSession, func()) {
+	driverConns := []*SQLiteConn{}
+	driverName := fmt.Sprintf("sqlite3_testSession_%s", test)
+	sql.Register(driverName, &SQLiteDriver{
+		ConnectHook: func(conn *SQLiteConn) error {
+			driverConns = append(driverConns, conn)
+			return nil
+		},
+	})
+
+	db, err := sql.Open(driverName, ":memory:")
+	if err != nil {
+		t.Fatal("Failed to open the session test database:", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		t.Fatal("Failed to connect to the session test database:", err)
+	}
+
+	conn := driverConns[0]
+	session, err := conn.Session("main")
+	if err != nil {
+		t.Fatal("Failed to create new session:", err)
+	}
+
+	cleanup := func() {
+		if err := session.Close(); err != nil {
+			t.Fatal("Failed to close test session:", err)
+		}
+		if err := db.Close(); err != nil {
+			t.Fatal("Failed to close the session test database:", err)
+		}
+	}
+
+	return db, session, cleanup
 }
